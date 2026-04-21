01edec0aa9e650899951cb7e76370bb960c47703
Micah Mahabir <mahabirm05915@gmail.com>
2026-04-20 22:53:03 -0400

    Delete original app directory

--- app/main.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-from fastapi import FastAPI
-from app.routers import main_router
-
-app = FastAPI()
-app.include_router(main_router)
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
--- app/routers/__init__.py
+++
@@ -1,2 +0,0 @@
-from fastapi import APIRouter
-main_router = APIRouter()
